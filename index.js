8eeb096580ed67fa62a2159eff3f0ecee93cc4db
Novel <novelmalla5@gmail.com>
2026-02-23 17:15:26 +0530

    Add express for Render

--- index.js
+++ index.js
@@ -2,6 +2,19 @@ require('dotenv').config();
 
 const { Client, GatewayIntentBits } = require('discord.js');
 
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('Valorant Bot Running 🎮');
+});
+
+app.listen(PORT, () => {
+  console.log(`Web server running on port ${PORT}`);
+});
+
 const client = new Client({
   intents: [
     GatewayIntentBits.Guilds,
